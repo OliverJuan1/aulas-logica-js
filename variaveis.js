f25d866483e5a41bd5d45463dabfdc46e3e2b742
OLIVER JUAN RODRIGUES DOS SANTOS <oliver_santos@estudante.sesisenai.org.br>
2025-04-16 22:18:59 -0300

    declaracao

---
+++ variaveis.js
@@ -0,0 +1,19 @@
+// declaração de variável com var
+
+// var nome = "Oliver"
+
+// var nome = "Raphael"
+
+// declaração de variável com let
+
+// let nome = "Oliver"
+
+// nome ="Raphael"
+
+// variável declarada com const
+
+const nome = "Oliver"
+
+nome = "Raphael"
+
+console.log(nome)
