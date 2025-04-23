1b46e6e4413ec199174d41b2f0e5cc6d5c752808
OLIVER JUAN RODRIGUES DOS SANTOS <oliver_santos@estudante.sesisenai.org.br>
2025-04-22 22:23:30 -0300

    feat:exercicios com node

--- variaveis.js
+++ variaveis.js
@@ -12,8 +12,47 @@
 
 // variável declarada com const
 
-const nome = "Oliver"
+const nome = "Oliver"  // string
 
-nome = "Raphael"
+const sobrenome = "Rodrigues"
 
-console.log(nome)
+const valor = 21.90  // number
+
+const islogin = false  // boolean
+
+let preco  // undefined
+
+const endereco = null  //null  
+
+const alunos = ["João", "Maria", "José"] //array
+
+const alunoDoSenai = {
+    nome: "Oliver",
+    sobrenome: "Rodrigues",
+    turno: "noturno",
+    idade: 16,
+    cursos: ["inclusão digital", "programação web"]
+}
+
+
+const alunosDoSenai = [
+    {
+        login:"oliver",
+        email:"oliver@gmail.com",
+        cursos:["games","web"]
+    },
+    {
+        login:"maria",
+        email:"maria@gmail.com",
+        cursos:["design","games"]
+    }
+]
+
+// console.log(nome + " " + sobrenome)
+
+console.log(`Meu nome completo é ${nome} ${sobrenome} ${4 + 4}`)
+
+
+// console.log(alunosDoSenai[1].cursos[0]) 
+
+// console.log(alunos[0]) //pega o valor do indice do array 
