f75f60d52c403752049ac344fe0264674d6cbd6b
OLIVER JUAN RODRIGUES DOS SANTOS <oliver_santos@estudante.sesisenai.org.br>
2025-04-23 22:15:13 -0300

    aprendendo mais sobre JS

--- exercicios02.js
+++ exercicios02.js
@@ -9,4 +9,4 @@ const produto02 ={
     preco: 30.00 ,
     quantidade: 2
 }
-console.log(`O valor total final é ${(produto01.preco * produto01.quantidade)+ (produto02.preco * produto02.quantidade)}`)
+console.log(`O resultado do cálculo final é ${(produto01.preco * produto01.quantidade)+ (produto02.preco * produto02.quantidade)}`)
