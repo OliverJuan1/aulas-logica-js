331b23ef7074a95fed02da0ddd3ecfb97ae153d1
OLIVER JUAN RODRIGUES DOS SANTOS <oliver_santos@estudante.sesisenai.org.br>
2025-04-28 22:09:30 -0300

    Aprendendo mais sobre JS

---
+++ condicionais.js
@@ -0,0 +1,20 @@
+// const idade = 18
+// const temCarteira = false
+
+// if(idade >= 18 || temCarteira){
+//     console.log("Pode Dirigir")
+// } else{
+//     console.log("Não pode dirigir")
+// }
+
+
+const notaDeMatematica = 8
+const frequencia = 9.0
+
+if(notaDeMatematica > 7 && frequencia >= 7.5){
+    console.log("Aprovado")
+} else if(notaDeMatematica >= 5 && frequencia >= 7.5){
+    console.log("Recuperação")
+} else{
+    console.log("Reprovado")
+}
---
+++ exercicios03.js
@@ -0,0 +1,11 @@
+// Crie uma função que recebe um parâmetro de nota e verfica em um condicional se a nota for maior que 7, o aluno está aprovado, senão, o aluno etá reprovado
+
+function resultadoFinal(nota) {
+    if (nota >= 7) {
+        console.log("Resultado Final é: Aprovado")
+    } else {
+        console.log("Resultado Final é: Reprovado")
+    }
+}
+resultadoFinal(7)
+
