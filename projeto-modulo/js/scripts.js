c0e44d68185263a7f515e9bf390e345c788e0c5d
OLIVER JUAN RODRIGUES DOS SANTOS <oliver_santos@estudante.sesisenai.org.br>
2025-05-12 22:12:29 -0300

    Arquitetura de módulos

---
+++ projeto-modulo/js/scripts.js
@@ -0,0 +1,16 @@
+import header from "./componente/header.js";
+
+
+header()
+
+
+// main
+import main from "./componente/main.js";
+
+main()
+
+
+// footer
+import footer from "./componente/footer.js";
+
+footer()
